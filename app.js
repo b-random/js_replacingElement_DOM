85935230e8dc3dbb7214b1a45a704c7d741571b8
b-random <brandon.brigance@gmail.com>
2022-08-09 10:23:57 -0700

    Event listeners

--- app.js
+++ app.js
@@ -1,67 +1,38 @@
-// REPLACE ELEMENT
-//replacing an h5 element with an h2 element:
+//Event Listeners
 
+// document.querySelector('.clear-tasks').addEventListener('click', function(objPara){
+//   console.log('hello!');
+//   objPara.preventDefault(); 
+// });
+//<a class="clear-tasks btn black" href="">Clear Tasks</a>
+//the href in the index needs a hashmark to hold the event, otherwise it just flashes
 
-//create element
-const newHeading = document.createElement('h2');
+document.querySelector('.clear-tasks').addEventListener('click', onClick);
 
-//add/replace ID
-newHeading.id = 'task-title';
+function onClick(e){
+//... console.log('clicked it');
+//... e.preventDefault(); //bcuz this href is set to google
 
-//add/replace text node
-newHeading.appendChild(document.createTextNode('Task List'));
-//The appendChild() method appends a node (element) as the last child of an element.
+//event targeting
+  let val;
+  val = e.target; //returns actual element <a...>
+  val = e.target.className;
+  val = e.target.classList.add('new-class');
+  val = e.target.className; //now with added class
+  val = e.target.classList;
+  val = e.target.innerText = 'pooshed!'; //changes button text after click
 
-//get the old heading
-const oldHeading = document.getElementById('task-title');
+  //event type
+  val = e.type;  //click
 
-//find parent to replace the child... using class in this case
-const findParent = document.querySelector('.card-action');
-//this is the div that the h5 will be removed from and the h2 will be replaced into
+  //timestamp
+  val = e.timeStamp;
 
-//replace
-findParent.replaceChild(newHeading, oldHeading);
-//first arguement is what is new element, second arguement is what is being replaced
-
-
-
-
-
-//now to remove an 'li' element
-const lis = document.getElementsByTagName('li');
-
-//lis[2].remove();
-lis[2].style.color = 'red';
-
-//or remove a child element
-// ......const list = document.querySelector('ul').children;
-// ..... list[2].remove();
-
-//or
-
-const listOther = document.querySelector('ul');
-listOther.removeChild(lis[2]);
-
-// move classes and attributes
-//classes..
-const firstLi = document.querySelector('li');
-const liKid = firstLi.children[0];
-
-val = liKid.className;
-val = liKid.classList; // //Dom token, so iterable 
-val = liKid.classList[0];
-liKid.classList.add('money-shot');
-val = liKid;
-liKid.classList.remove('money-shot');
-val = liKid.classList;
-
-//attributes...
-val = liKid.getAttribute('href'); //#
-val = liKid.setAttribute('href', 'http://google.com');
-val = liKid.setAttribute('title', 'google');
-val = liKid.getAttribute('href'); // google.com
-val = liKid.hasAttribute('href'); // true
-val = liKid.getAttribute('title'); // 'google'
-val = liKid
-
-console.log(val);
+  //event coordinates; the exact spot where the selected element is clicked relative to window
+  val = e.clientX;
+  val = e.clientY;
+  //event coordinates; the exact spot where the selected element is clicked relative to window
+  val = e.offsetY;
+  
+  console.log(val);
+};
