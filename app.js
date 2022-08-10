815a879fb28b33bd9b691fff3351cd4161110bf1
b-random <brandon.brigance@gmail.com>
2022-08-10 15:18:05 -0700

    Keyboard/Input DOM events

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-//Mouse Events
+//Keyboard/Input Events
 
-const clearBtn = document.querySelector('.clear-tasks');
-const card = document.querySelector('.card');
-const heading = document.querySelector('h5');
+const form = document.querySelector('form');
+const taskInput = document.getElementById('task');
 
-//click and mouse events
-// clearBtn.addEventListener('click', runEvent);
-// clearBtn.addEventListener('dblclick', runEvent);
-//clearBtn.addEventListener('mouseup', runEvent); 
-//heading.addEventListener('mouseenter', runEvent);
-//card.addEventListener('mouseenter', runEvent);
+taskInput.value = '';
+//the html has a value of 'Walk the Dog' and this removes it
 
-//mousemove... good for gaming/interactive applications
-card.addEventListener('mousemove', runEvent);
 
-//track mouse movement within an element
-//event handler
+
+// form.addEventListener('submit', runEvent);
+
+// function runEvent(e){
+//   console.log(`Event Type: ${e.type}`); 
+//   // .type fires on event addEventListener and returns string
+//   console.log(taskInput.value);
+//   //returns submitted value to console
+//   e.preventDefault();
+//   //this prevents the default behavior of an html form, which is to refresh after submission 
+//   taskInput.value = '';
+// };
+
+
+const newHead = document.querySelector('h5');
+//taskInput.addEventListener('keydown', runEvent);
+//taskInput.addEventListener('keyup', runEvent);
+taskInput.addEventListener('focus', runEvent);
+
+
+
 function runEvent(e){
-  console.log(`Event Type: ${e.type}`);
-  heading.textContent = `MouseX: ${e.offsetX} MouseY ${e.offsetY}`;
-
-  document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 255)`;
-}
+  console.log(`Event Type: ${e.type}`); 
+  // .type fires on event addEventListener and returns string
+  //console.log(e.target.value);
+  // e references the eventListener through the function and .target references what the listener is being called on, in this case the input element
+  //e.preventDefault();
+  //....newHead.innerText = e.target.value;
+  console.log(taskInput.value);
+};
